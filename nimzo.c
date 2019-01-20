41407a632516639ac82a6da280e0334395cd5842
otsoga <victor.m.agosto@gmail.com>
2019-01-20 01:56:00 -0500

    inital commit

---
+++ nimzo.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+#define WK 1
+#define WQ 2
+#define WR 3
+#define WB 4
+#define WN 5
+#define WP 6
+#define BK 7
+#define BQ 8
+#define BR 9
+#define BB 10
+#define BN 11
+#define BP 12
+#define WHITE 1
+#define BLACK 2
+
+void drawSquare(int value);
+void drawBoard(int board[8][8]);
+void movePiece(int from[2], int to[2], int board[8][8]);
+int makeMove(char move[5], int board[8][8], int turn);
+
+int main(void)
+{
+    int to[2];
+    int from[2];
+    int board[8][8] = {
+        {WR, WN, WB, WQ, WK, WB, WN, WR},
+        {WP, WP, WP, WP, WP, WP, WP, WP},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {BP, BP, BP, BP, BP, BP, BP, BP},
+        {BR, BN, BB, BQ, BK, BB, BN, BR}
+    };
+    
+    makeMove("e2-e4", board, WHITE);
+    makeMove("e7-e5", board, BLACK);
+    makeMove("g1-f3", board, WHITE);
+    makeMove("b8-c6", board, BLACK);
+    makeMove("f1-b5", board, WHITE);
+    drawBoard(board);
+
+    return 0;
+}
+
+void drawBoard(int board[8][8])
+{
+    for (int i = 7; i > -1; i--) {
+        for(int j = 0; j < 8; j++) {
+            drawSquare(board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void drawSquare(int value) 
+{
+    char piece[13];
+    piece[WK] = 'K';
+    piece[WQ] = 'Q';
+    piece[WR] = 'R';
+    piece[WB] = 'B';
+    piece[WN] = 'N';
+    piece[WP] = 'P';
+    piece[BK] = 'k';
+    piece[BQ] = 'q';
+    piece[BR] = 'r';
+    piece[BB] = 'b';
+    piece[BN] = 'n';
+    piece[BP] = 'p';
+    piece[0]  = '.';
+    
+    printf("%c", piece[value]);
+}
+
+
+
+int makeMove(char move[5], int board[8][8], int turn)
+{
+    int to[2];
+    int from[2];
+
+    if (move[0] >= 'a' && move[0] <= 'h') {
+        from[1] = (int) (move[0] - 'a');
+        from[0] = (int) (move[1] - '0' - 1);
+        to[1] = (int) (move[3] - 'a');
+        to[0] = (int) (move[4] - '0' - 1);
+    }
+
+    movePiece(from, to, board);
+
+    return 1;
+}
+
+void movePiece(int from[2], int to[2], int board[8][8]) {
+    board[to[0]][to[1]] = board[from[0]][from[1]];
+    board[from[0]][from[1]] = 0;
+}
